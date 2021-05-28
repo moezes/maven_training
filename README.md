b4d3a04c198b2a3eec92fa8a5d0234828bf1c009
Mohamad AL HAWARI <mohamad.al-hawari@epita.fr>
2021-05-28 17:38:38 +0200

    Add live badges

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Maven Training
 
+![Build CI](https://github.com/moezes/maven_training/actions/workflows/build.yml/badge.svg)
+
+[![codecov](https://codecov.io/gh/moezes/maven_training/branch/main/graph/badge.svg?token=3ff99c54-5117-4b5e-b1a4-022bb402174b)](https://codecov.io/gh/moezes/maven_training)
+
 Hello. This repository is dedicated for the Java course's Maven exercise.
 
 * [X] Partie 0
@@ -8,4 +12,4 @@ Hello. This repository is dedicated for the Java course's Maven exercise.
 * [X] Partie 3 - Maven wrapper
 * [X] Partie 4 - CI
 * [X] Partie 5 - Code coverage
-* [ ] Partie 6 - Live badges
+* [X] Partie 6 - Live badges
--- src/test/java/fr/lernejo/SampleTest.java
+++ src/test/java/fr/lernejo/SampleTest.java
@@ -6,16 +6,19 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SampleTest {
+
+    private final Sample sample = new Sample();
+
     @Test
     void fact_negative_exception() {
         int number = -1;
-        Assertions.assertThatIllegalArgumentException().isThrownBy(() -> Sample.fact(number));
+        Assertions.assertThatIllegalArgumentException().isThrownBy(() -> sample.fact(number));
     }
 
     @Test
     void fact_5_is_120() {
         int input = 5;
-        int res = Sample.fact(5);
+        int res = sample.fact(5);
         Assertions.assertThat(res).as("facto of 5").isEqualTo(120);
     }
 
@@ -24,20 +27,20 @@ class SampleTest {
         int addend1 = 9;
         int addend2 = 10;
         Sample.Operation operand = Sample.Operation.ADD;
-        int sum = Sample.op(operand, addend1, addend2);
+        int sum = sample.op(operand, addend1, addend2);
         Assertions.assertThat(sum).as("sum of 9 + 10").isEqualTo(19);
     }
 
     @Test
     void mult_3_7_is_21() {
-        int res = Sample.op(Sample.Operation.MULT, 3, 7);
+        int res = sample.op(Sample.Operation.MULT, 3, 7);
         Assertions.assertThat(res).as("product of 3 by 7").isEqualTo(21);
     }
 
     @Test
     void mult_by_0() {
         int multiplier = 21;
-        int res = Sample.op(Sample.Operation.MULT, multiplier, 0);
+        int res = sample.op(Sample.Operation.MULT, multiplier, 0);
         Assertions.assertThat(res).as("multiplication by 0").isZero();
 
         // can we add several asserts in one?
